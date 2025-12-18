f1d7e183a97abf473ad7df8f905538643d57c690
Gavan Bess <gavanbess@gmail.com>
2025-12-17 18:42:22 -0800

    space needle

---
+++ CustomAsciiArt.java
@@ -0,0 +1,12 @@
+// Gavan Bess
+// 12/17/2025
+// AP CS A
+// Lab #2a - Custom Ascii Art
+//
+// This program prints Moon phase ascii art to console
+
+public class CustomAsciiArt {
+    public static void main(String[] args) {
+        
+    }
+}
--- SpaceNeedle.java
+++ SpaceNeedle.java
@@ -1,3 +1,10 @@
+// Gavan Bess
+// 12/17/2025
+// AP CS A
+// Lab #2b - Space Needle
+//
+// This program prints a scalable representation of the Seattle Space Needle to console
+
 import java.lang.Math;
 import java.lang.Runnable;
 
@@ -7,7 +14,8 @@ public class SpaceNeedle {
     public static final int NEEDLE_HEIGHT = SIZE;
     public static final int NUM_STAGES_TOP_BOTTOM_BASE = SIZE; // number of stages in the base, and the top and bottom of the "ufo" part
     public static final int MAX_WIDTH = 2 + (SIZE * 6);
-    public static final int ELEVATOR_HEIGHT = (int) Math.pow(2, SIZE);
+    public static final int ELEVATOR_HEIGHT = SIZE * SIZE;
+    public static final int ELEVATOR_WIDTH = Math.max(SIZE - 2, 0);
 
     private static void repeat(int num, Runnable thing) {
         for (int i = 0; i < num; i++) {
@@ -17,13 +25,57 @@ public class SpaceNeedle {
 
     public static void main(String[] args) {
         printAntenna();
+        printUFOTop();
+        printUFOBottom();
+        printAntenna();
+        printElevator();
+        printUFOTop();
     }
 
     public static void printAntenna() {
-        repeat(NEEDLE_HEIGHT, () -> {System.out.println(" ".repeat((MAX_WIDTH-2)/2).concat("||"));});
+        repeat(NEEDLE_HEIGHT, () -> {
+            System.out.println(" ".repeat((MAX_WIDTH - 2) / 2).concat("||"));
+        });
     }
-    
-    public static void printUFOThing() {
-        
+
+    public static void printElevator() {
+        repeat(ELEVATOR_HEIGHT, () -> {
+            System.out.println(
+                " ".repeat((MAX_WIDTH - (SIZE * 2)) / 2).concat(
+                    "|".concat("%".repeat(ELEVATOR_WIDTH)).concat("|").repeat(2)
+                )
+            );
+        });
+    }
+
+    public static void printUFOTop() {
+        for (int i = 0; i < SIZE; i++) {
+            int spaces = (MAX_WIDTH / 2) - (4 + (3 * i));
+            String side = "__/".concat(":".repeat(i * 3)).concat("|");
+            System.out.println(
+                " ".repeat(spaces)
+                    .concat(side)
+                    .concat(
+                        new StringBuilder(side)
+                            .replace(2, 3, "\\")
+                            .reverse()
+                            .toString()
+                    )
+            );
+        }
+
+        System.out.println("|".concat("\"".repeat(MAX_WIDTH - 2)).concat("|"));
+    }
+
+    public static void printUFOBottom() {
+        for (int i = 0; i < SIZE; i++) {
+            System.out.println(
+                " ".repeat(i * 2).concat(
+                    "\\_".concat(
+                        "/\\".repeat(((MAX_WIDTH - 4) - (i * 4)) / 2)
+                    ).concat("_/")
+                )
+            );
+        }
     }
 }
