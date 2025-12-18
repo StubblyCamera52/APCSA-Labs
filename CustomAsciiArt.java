21a73dba1edde692ecb7699193c4f49b91bc04f1
Gavan Bess <gavanbess@gmail.com>
2025-12-18 11:18:31 -0800

    formatting on custom ascii art

--- CustomAsciiArt.java
+++ CustomAsciiArt.java
@@ -5,10 +5,10 @@
 //
 // This program prints Moon phase ascii art to console
 
-import java.util.Timer;
-import java.util.TimerTask;
 import java.lang.Runtime;
 import java.lang.Thread;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class CustomAsciiArt {
 
@@ -26,26 +26,33 @@ public class CustomAsciiArt {
     static volatile int frame = 0;
 
     public static void main(String[] args) {
-        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-            System.out.println("\u001b[?25h");
-        }));
-
+        Runtime.getRuntime().addShutdownHook(
+            new Thread(() -> {
+                System.out.println("\u001b[?25h");
+            })
+        );
 
         System.out.print("\u001b[?25l");
+
         System.out.println();
         System.out.println();
         System.out.println();
         System.out.println();
         System.out.println();
         System.out.print("\u001b[4A\u001b[20C");
+
         Timer timer = new Timer();
-        timer.schedule(new TimerTask() {
-            @Override
-            public void run() {
-                System.out.print(MOON_ART[frame%8]);
-                System.out.print("\u001b[2A\u001b[5D");
-                frame++;
-            }
-        }, 0, 125);
+        timer.schedule(
+            new TimerTask() {
+                @Override
+                public void run() {
+                    System.out.print(MOON_ART[frame % 8]);
+                    System.out.print("\u001b[2A\u001b[5D");
+                    frame++;
+                }
+            },
+            0,
+            125
+        );
     }
 }
